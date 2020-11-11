a5b1039355a8a76649529c9b10f4a1b7417d6897
r00t0k <artists2@naver.com>
2020-11-12 02:08:18 +0900

    Add insertDB

--- DB/dbCtrl.py
+++ DB/dbCtrl.py
@@ -11,18 +11,25 @@ translationFile_Dict = {
 class dbController:
     def __init__(self, host, user, password, db, charset):
         self.dbconn = pymysql.connect(host = host, user = user, password = password, db = db, charset = charset)
-        self.dbcursor = self.dbconn.cursor()
-    def insertDB(self, table, ):
+        self.dbcursor = self.dbconn.cursor(pymysql.cursors.DictCursor)
+    def insertDB(self, tableName, values):
+        print("******insert DB")
+        self.dbcursor.execute("INSERT INTO " + tableName + " VALUES " +  str(values))
+        self.dbconn.commit()
         pass
     def deleteDB(self):
         pass
-    def selectDB(self):
+    def selectDB(self, tableName, fieldName, condString):
+        print("SELECT " + fieldName + " FROM " + tableName + " WHERE " + condString)
+        self.dbcursor.execute("SELECT " + fieldName + " FROM " + tableName + " WHERE " + condString)
+        result = self.dbcursor.fetchone() #fetchall, fetchone
+        return result
+        #return self.dbcursor.fetchall()
         pass
     def updateDB(self):
         pass
 
-dbC = dbController(**translationFile_Dict)
 
-dbC.insertDB()
-"""dbC = dbController()
-print(type(dbC))"""
+#dbC = dbController(**translationFile_Dict)
+#dbC.insertDB("user", ("a", "b", "c"))
+#dbC.selectDB("user", "*", "user_id = 'test123'")
