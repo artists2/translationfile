404c3772bc589d234d2d8efe40b34950d58f28a8
r00t0k <artists2@naver.com>
2020-11-11 15:43:19 +0900

    Add PyMySQL

--- DB/dbCtrl.py
+++ DB/dbCtrl.py
@@ -1,10 +1,28 @@
-import pymysql #host='localhost', port=3306, user='root', passwd='maria', db='estdb',charset='utf8',autocommit=True)
+import pymysql #host='localhost', port=3306, user='root', passwd='mysql1234', db='estdb',charset='utf8',autocommit=True)
+
+translationFile_Dict = {
+    "user" : "root",
+    "password" : "mysql1234",
+    "host" : "127.0.0.1",
+    "db" : "translationfile",
+    "charset" : "utf8"
+}
 
 class dbController:
-    def __init__(self, host, id, pw, db_name):
-        self.dbconn = pymysql.connect(host = host, user = id, password = pw, db = db_name, charset = 'utf8')
+    def __init__(self, host, user, password, db, charset):
+        self.dbconn = pymysql.connect(host = host, user = user, password = password, db = db, charset = charset)
         self.dbcursor = self.dbconn.cursor()
-    def insertDB(self):
+    def insertDB(self, table, ):
         pass
     def deleteDB(self):
         pass
+    def selectDB(self):
+        pass
+    def updateDB(self):
+        pass
+
+dbC = dbController(**translationFile_Dict)
+
+dbC.insertDB()
+"""dbC = dbController()
+print(type(dbC))"""
